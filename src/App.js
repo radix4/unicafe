e8d9381037cf165405542640a2642a451cfbca1c
Thang Cao <shiyoline@gmail.com>
2021-01-06 09:58:26 -0800

    refactor statistics

--- src/App.js
+++ src/App.js
@@ -23,18 +23,34 @@ const Statistic = ({ feedback, count }) => {
   )
 }
 
-const Statistics = props => {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = ((good - bad) / all).toFixed(1)
+  const positive = ((good / all) * 100).toFixed(1)
+
+  if (all === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        no feedback given
+      </div>
+    )
+  }
+
   return (
-    <table>
-      <tbody>
-        <Statistic feedback='good' count={props.good} />
-        <Statistic feedback='neutral' count={props.neutral} />
-        <Statistic feedback='bad' count={props.bad} />
-        <Statistic feedback='all' count={props.allFeedback} />
-        <Statistic feedback='average' count={props.average} />
-        <Statistic feedback='positive' count={props.positive} percentage='%' />
-      </tbody>
-    </table>
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <Statistic feedback='good' count={good} />
+          <Statistic feedback='neutral' count={neutral} />
+          <Statistic feedback='bad' count={bad} />
+          <Statistic feedback='all' count={all} />
+          <Statistic feedback='average' count={average} />
+          <Statistic feedback='positive' count={positive} percentage='%' />
+        </tbody>
+      </table>
+    </div>
   )
 }
 
@@ -57,24 +73,6 @@ const App = () => {
     setAll(allFeedback + 1)
   }
 
-  const average = ((good - bad) / allFeedback).toFixed(1)
-  const positive = ((good / allFeedback) * 100).toFixed(1)
-
-  if (allFeedback === 0) {
-    return (
-      <div>
-        <h1>give feedback</h1>
-        <Buttons
-          incrementGood={incrementGood}
-          incrementNeutral={incrementNeutral}
-          incrementBad={incrementBad}
-        />
-        <h1>statistics</h1>
-        <h1>no feedback given</h1>
-      </div>
-    )
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -83,15 +81,7 @@ const App = () => {
         incrementNeutral={incrementNeutral}
         incrementBad={incrementBad}
       />
-      <h1>statistics</h1>
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        allFeedback={allFeedback}
-        average={average}
-        positive={positive}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
